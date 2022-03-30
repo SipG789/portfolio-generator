d0e33b8e56ee474fca9b5683e27949247adefe86
Sip <sipg789@gmail.com>
2022-03-30 14:47:06 -0500

    generate webpage

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
 
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-    console.log('================');
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
